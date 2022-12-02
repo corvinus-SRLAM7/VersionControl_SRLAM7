0202d533cb86492a3171e79d19af721c24bb2312
Zoltán András Suller <zoltan.suller@stud.uni-corvinus.hu>
2022-12-02 10:13:41 +0100

    Osztályok létrehozása

---
+++ SRLAM7_kilencedikgyak_2/Entities/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRLAM7_kilencedikgyak_2.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Felmale = 2
+    }
+}
